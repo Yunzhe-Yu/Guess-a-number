1e16a3de8d5066a5db5074f382b570b2b5c781b9
Yunzhe Yu <yunzheyu0529@gmail.com>
2020-12-30 20:30:33 -0800

    add new function

--- number.py
+++ number.py
@@ -1,11 +1,14 @@
 import random
 r = random.randint(1,10)
-
+times = 0
 while True:
 	number = input('Please enter a number you guess:')
 	number = int(number)
+	times = times + 1
 	if number == r:
-		print('You win!')
+		print('You win! You total use ', times, 'times!')
 		break
+	elif number < r:
+		print('You wrong! Smaller than the answer! This is your ', times, 'times! Please enter it again:')
 	else:
-		print('You wrong! Please enter it again:')
+		print('You wrong! Bigger than the answer! This is your ', times, 'times! Please enter it again:')
