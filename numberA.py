a9351dd7e64bd6185cbf8934a76ef112082bf3e0
Yunzhe Yu <yunzheyu0529@gmail.com>
2020-12-30 20:45:23 -0800

    update the file

--- numberA.py
+++ numberA.py
@@ -1,13 +1,18 @@
 import random
 
 r = random.randint(1,100)
+count = 0
+
 while True:
+	count += 1 # count = count + 1
 	num = input('Please enter a number:')
 	num = int(num) #change the datatype
 	if num == r:
 		print('You win!')
+		print('This is your ', count, 'times!')
 		break
 	elif num < r:
 		print('Small!')
 	else:
 		print('Big!')
+	print('This is your ', count, 'times!')
